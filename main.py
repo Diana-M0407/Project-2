6d8d13ab2d81b98e6b336e100b5467c83000316c
Diana Maldonado <di.maldonado5504@csu.fullerton.edu>
2025-03-27 00:36:05 -0700

    Added Algorithm 2 to main.py, updated output for output_1.txt file

--- main.py
+++ main.py
@@ -33,13 +33,55 @@ array_pairs = re.findall(r'array \d+a\s*=\s*(\[".*?"\])\s*array \d+b\s*=\s*(\[.*
 
 # Process each pair
 with open("output_1.txt", "w") as file:
+  file.write(f"Results from Algorithm 1: \"Target Terms or Substrings\"\n\n")
+
   for i, (raw_A, raw_B) in enumerate(array_pairs, start=1):
       A = eval(raw_A)  # array_A: one string inside a list
       B = eval(raw_B)  # array_B: list of city names
       output_order, output_array = find_cities_by_index(A, B)
 
+
       file.write(f"Array {i}a and {i}b:\n")
       file.write(f"Output_order = {output_order}\n")
       file.write(f"Output_array = {output_array}\n\n")
 
-print("Results written to output_1.txt\n")
+print("\nResults from Algorithm 1 written to output_1.txt\n")
+
+
+
+
+# Algorithm 2: Run Encoding Problem
+
+def run_length_encoding(input_string):
+    #Initialize an empty list to return and char_position of input_string
+    encoded_list = []
+    char_position = 0
+
+    while char_position < len(input_string):
+        #Initialize count for the current char
+        count = 1
+
+        #Count occurrences of the current char
+        while char_position + 1 < len(input_string) and input_string[char_position] == input_string[char_position + 1]:
+            count += 1
+            char_position += 1
+
+        #Append the encoded result
+        if count > 1:
+            encoded_list.append(str(count) + input_string[char_position])
+        else:
+            encoded_list.append(input_string[char_position])
+
+        #Next new char in input_string
+        char_position += 1
+
+    return "".join(encoded_list)
+
+
+#Sample Test Cases
+print("Input: ddd")
+print("Output: " + run_length_encoding("ddd"))  #"3d"
+print("Input: heloooooooo there")
+print("Output: " + run_length_encoding("heloooooooo there"))  #"hel8o there"
+print("Input: choosemeeky and tuition-free")
+print("Output: " + run_length_encoding("choosemeeky and tuition-free"))  #"ch2osem2eky and tuition-fr2e"
