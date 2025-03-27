c673789a4baa0bdc4af06fdb36f2edcbf1bf2774
Diana Maldonado <di.maldonado5504@csu.fullerton.edu>
2025-03-26 20:55:19 -0700

    added test arrays for find_cities_by_index() function in main

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
 # c. Implement your algorithm in either Python or C++
 
-#from docx import Document
 from docx import Document
 
 
 doc = Document("in2a.docx")
 doc_string = " ".join([para.text for para in doc.paragraphs])
 
+array_1a = ["sanoaklandrialtofullertonmarcolongbreacoronamodestoclovissimithousand"]
+array_1b = ['brea', 'modesto', 'clovis', 'corona']
+
+
+array_2a = ["marcopolmonafremontrialtofullertonmarcolongfresnochinoclovissimisalinas"]
+array_2b = ['fullerton', 'chino', 'fremont', 'fresno']
+
+array_3a = ["torranceoaklandrialtomarcooxnardchinofresnoirvineclovissimiorange"]
+array_3b = ['oxnard','irvine', 'orange', 'marco']
 
 #Input:
 A = [ "thismetoaklandrialtofullertonmarcolongchinofresnovallejoclovissimithound"]
@@ -49,11 +57,15 @@ def find_cities_by_index(array_A, array_B):
     output_order.append(idx)
     output_array.append(val)
 
-  print("Output_order =", output_order)
-  print("Output_array =", output_array)
+
+  print(f"Output_order = {output_order}\n")
+  print(f"Output_array = {output_array}\n")
 
   return output_order                                                   # returns index values in order of appearance
 
 
 find_cities_by_index(A, B)
-print(doc_string)
+find_cities_by_index(array_1a, array_1b)
+find_cities_by_index(array_2a, array_2b)
+find_cities_by_index(array_3a, array_3b)
+#print(doc_string)
