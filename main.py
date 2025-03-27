117a6b0adf61c082faec2d877583befb801ab347
Diana Maldonado <di.maldonado5504@csu.fullerton.edu>
2025-03-26 23:05:28 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -1,39 +1,18 @@
-# c. Implement your algorithm in either Python or C++
+# Algorithm 1: Target Terms or Substrings
 
 import re   #import read/write/run
 from docx import Document # import document
 
-#Input:
-A = [ "thismetoaklandrialtofullertonmarcolongchinofresnovallejoclovissimithound"]
-B = [ 'marco', 'clovis', 'rialto', 'oakland']
-
-#Docx:
-array_1a = ["sanoaklandrialtofullertonmarcolongbreacoronamodestoclovissimithousand"]
-array_1b = ['brea', 'modesto', 'clovis', 'corona']
-
-
-array_2a = ["marcopolmonafremontrialtofullertonmarcolongfresnochinoclovissimisalinas"]
-array_2b = ['fullerton', 'chino', 'fremont', 'fresno']
-
-array_3a = ["torranceoaklandrialtomarcooxnardchinofresnoirvineclovissimiorange"]
-array_3b = ['oxnard','irvine', 'orange', 'marco']
-
-
 def find_cities_by_index(array_A, array_B):
   cities = {}
 
-  # m = length of array_A
-  # n = length of array_B
-  # s = number of characters of string, array_A[0]
-
   string = array_A[0]
-  for city in array_B:                                                  # ----> Efficiency Class:  O(m) - "Linear"
-    index = string.find(city)                                           # ----> Efficiency Class:  O(s) - "Linear"
+  for city in array_B:
+    index = string.find(city)
     if (index != -1):
         cities[index] = city
 
-
-  sorted_cities = dict(sorted(cities.items()))                          # -----> Efficiency Class: k log k - "Logarithmic"
+  sorted_cities = dict(sorted(cities.items()))
 
   output_order = list(sorted_cities.keys())
   output_array = list(sorted_cities.values())
@@ -41,7 +20,7 @@ def find_cities_by_index(array_A, array_B):
   print(f"Output_order = {output_order}")
   print(f"Output_array = {output_array}")
 
-  return output_order                                                   # returns index values in order of appearance
+  return output_order
 
 
 
@@ -61,4 +40,3 @@ for i, (raw_A, raw_B) in enumerate(array_pairs, start=1):
     B = eval(raw_B)  # array_B: list of city names
     print(f"\n▶ Matching array {i}a and {i}b")
     find_cities_by_index(A, B)
-
