4992383631672630754064cb56f6eef2a9578225
Diana Maldonado <di.maldonado5504@csu.fullerton.edu>
2025-03-27 23:22:20 -0700

    Updated main.py

--- main.py
+++ main.py
@@ -22,8 +22,10 @@ def find_cities_by_index(array_A, array_B):
 
 
 
-# Algorithm 2: Run Encoding Problem
-# This run-length encoding compresses any character repeated 2 or more times.
+# Algorithm 2: Run-Length Encoding
+# This function compresses any character that appears consecutively 2 or more times
+# by replacing the sequence with the count followed by the character.
+# Single characters are left unchanged.
 def run_length_encoding(input_string):
     #Initialize an empty list to store encoded strings and char_position of input_string
     encoded_list = []
@@ -56,7 +58,7 @@ def extract_text(docx_file):
   doc = Document(docx_file)
   full_text = " ".join([para.text for para in doc.paragraphs])
 
-  # Replace fancy quotes with regular quotes
+  #Replace fancy quotes with regular quotes
   normalized_text = full_text.replace("“", '"').replace("”", '"').replace("‘", "'").replace("’", "'")
 
   return normalized_text
@@ -75,8 +77,9 @@ array_tuples = create_array_pairs(normalized_text)
 
 # Process each pair of arrays
 with open("output.txt", "w") as file:
-  file.write(f"{'='*10} Results from Algorithm 1: \"Target Terms or Substrings\" {'='*10}\n\n")
 
+  # Algorithm 1 output.txt:
+  file.write(f"{'='*15} Results from Algorithm 1: \"Target Terms or Substrings\" {'='*15}\n\n")
   for i, (raw_A, raw_B) in enumerate(array_tuples, start=1):
     try:
       A = ast.literal_eval(raw_A)
@@ -87,62 +90,24 @@ with open("output.txt", "w") as file:
 
     output_order, output_array = find_cities_by_index(A, B)
 
-
-    file.write(f"Array {i}a and {i}b:\n")
     file.write(f"Input: {i}a = {raw_A}\n")
     file.write(f"       {i}b = {raw_B}\n\n")
     file.write(f"Output: Output_order = {output_order}\n")
-    file.write(f"        Output_array = {output_array}\n\n")
+    file.write(f"        Output_array = {output_array}\n\n\n")
+
+  print("\nResults from Algorithm 1 written to output.txt\n")
 
 
-  file.write(f"{'='*10} Results from Algorithm 2: \"Run Encoding Problem\" {'='*10}\n\n")
+  # Algorithm 2 output.txt:
+  file.write(f"{'='*15} Results from Algorithm 2: \"Run Encoding Problem\" {'='*15}\n\n")
   for i, (raw_A, raw_B) in enumerate(array_tuples, start=1):
-    '''
-    try:
-      A = ast.literal_eval(raw_A)
-      B = ast.literal_eval(raw_B)
-    except(ValueError,SyntaxError) as e:
-       file.write(f"Error parsing inputs on line {i}: {e}\n")
-       continue
-    '''
 
     long_string = run_length_encoding(raw_A)
     short_string = run_length_encoding(raw_B)
 
-    #file.write(f"Array {i}a and {i}b:\n")
+    file.write(f"Array {i}a: ")
     file.write(f"{raw_A} becomes {long_string}\n")
+    file.write(f"Array {i}b: ")
     file.write(f"{raw_B} becomes {short_string}\n\n")
 
-print(raw_A)
-print(raw_B)
-print(run_length_encoding(raw_A))
-print(run_length_encoding(raw_B))
-print(A)
-print(B)
-
-
-print("\nResults from Algorithm 1 written to output_1.txt\n")
-
-
-
-
-
-
-
-
-
-
-
-
-
-#Sample Test Cases
-print("Input: ddd")
-print("Output: " + run_length_encoding("ddd"))  #"3d"
-print("Input: heloooooooo there")
-print("Output: " + run_length_encoding("heloooooooo there"))  #"hel8o there"
-print("Input: choosemeeky and tuition-free")
-print("Output: " + run_length_encoding("choosemeeky and tuition-free"))  #"ch2osem2eky and tuition-fr2e"
-
-#print(f"Printing nomalized_text: {normalized_text}\n")
-#print(f"printing array_tuples:  {array_tuples}\n")
-#print(run_length_encoding(normalized_text))
+  print("\nResults from Algorithm 2 written to output.txt\n")
